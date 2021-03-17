8a250fce6b5653d7bcb2a38e1523a640af98f3e1
Chernenko Dmitriy <d.chernenko@corp.mail.ru>
2021-03-17 13:29:55 +0300

    initial version: 0.01

--- lib/Crypt/xxHash.pm
+++ lib/Crypt/xxHash.pm
@@ -1,12 +1,30 @@
-# perl_crypt_xxhash
+package Crypt::xxHash;
+use strict;
+use base qw(Exporter);
+use Config ();
+use XSLoader;
 
-### NAME
+BEGIN {
+    our $VERSION = '0.01';
+    XSLoader::load __PACKAGE__, $VERSION;
+}
+
+our @EXPORT_OK = qw[
+	xxhash32       xxhash32_hex
+    xxhash64       xxhash64_hex
+    xxhash3_64bits xxhash3_64bits_hex
+                   xxhash3_128bits_hex
+];
+
+1;
+__END__
+
+=head1 NAME
 
 Crypt::xxHash - xxHash implementation for Perl
 
-### SYNOPSIS
+=head1 SYNOPSIS
 
-```perl
     use Crypt::xxHash qw[xxhash32 xxhash32_hex xxhash64 xxhash64_hex xxhash3_64bits xxhash3_64bits_hex xxhash3_128bits_hex];
 
     my $hash = xxhash32( $data, $seed );
@@ -19,9 +37,8 @@ Crypt::xxHash - xxHash implementation for Perl
     my $hex_64  = xxhash3_64bits_hex( $data, $seed );
 
     my $hex_128 = xxhash3_128bits_hex( $data, $seed );
-```
 
-### DESCRIPTION
+=head1 DESCRIPTION
 
 xxHash is s super fast algorithm that claims to work at speeds close to RAM limits.
 This package provides 32- and 64-bit hash functions.
@@ -31,15 +48,15 @@ This package was inspired by C<Digest::xxHash>, but it includes the fresh C code
 some optimisations. Thus all hex methods are implemented in Perl XS.
 Also this module doesn't use C<Math::Int64> in favor of native 64 bit digits.
 
-##### $h = xxhash32( $data, $seed )
+=head2 $h = xxhash32( $data, $seed )
 
 Returns a 32 bit hash.
 
-##### $h = xxhash32_hex( $data, $seed )
+=head2 $h = xxhash32_hex( $data, $seed )
 
 Returns a 32 bit hash converted into hex string.
 
-##### $h = xxhash64( $data, $seed )
+=head2 $h = xxhash64( $data, $seed )
 
 Returns a 64 bit hash.
 
@@ -47,26 +64,26 @@ Returns a 64 bit hash.
 
 Returns a 64 bit hash converted into hex string
 
-##### $h = xxhash3_64bits( $data, $seed )
+=head2 $h = xxhash3_64bits( $data, $seed )
 
 Returns a 64 bit hash which calculated by using xxHash3 algorithm.
 
-##### $h = xxhash3_64bits_hex( $data, $seed )
+=head2 $h = xxhash3_64bits_hex( $data, $seed )
 
 Returns a 64 bit hash which calculated by using xxHash3 algorithm.
 This hash is converted into hex string.
 
-##### $h = xxhash3_128its_hex( $data, $seed )
+=head2 $h = xxhash3_128its_hex( $data, $seed )
 
 Returns a 128 bit hash which calculated by using xxHash3 algorithm.
 This hash is converted into hex string.
 
-### SPEED
+=head1 SPEED
 
 There are some official benchmark results can be found on the project
 web-site L<https://github.com/Cyan4973/xxHash>
 
-### Benchmarks
+=head1 BENCHMARKS
 
 Below you can find some benchmarks in comparison with C<Digest::xxHash>.
 
@@ -75,7 +92,6 @@ so they have the same throughput capacity.
 
 But other methods have some differences:
 
-```
                                   Rate Digest::xxHash::xxhash32_hex Crypt::xxHash::xxhash32_hex                                                                                
 Digest::xxHash::xxhash32_hex 2577320/s                           --                        -54%                                                                                
 Crypt::xxHash::xxhash32_hex  5543237/s                         115%                          --
@@ -87,14 +103,15 @@ Crypt::xxHash::xxhash64  14893617/s                    7283%                    
                                   Rate Digest::xxHash::xxhash64_hex Crypt::xxHash::xxhash64_hex
 Digest::xxHash::xxhash64_hex  185048/s                           --                        -96%
 Crypt::xxHash::xxhash64_hex  4926108/s                        2562%                          --
-```
 
-### LICENSE
+=head1 LICENSE
 
 xxHash is covered by the BSD license.
 
-### AUTHOR
+=head1 AUTHOR
 
 Chernenko Dmitiry cdn@cpan.org
 
 xxHash by Yann Collet.
+
+=cut
