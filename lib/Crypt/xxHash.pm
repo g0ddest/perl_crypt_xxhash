2d08f926052d3deafc75c86f6288e5c9335c1dc7
Chernenko Dmitriy <d.chernenko@corp.mail.ru>
2021-03-17 13:38:30 +0300

    changes in POD

--- lib/Crypt/xxHash.pm
+++ lib/Crypt/xxHash.pm
@@ -44,7 +44,7 @@ Crypt::xxHash - xxHash implementation for Perl
 
 =head1 DESCRIPTION
 
-xxHash is s super fast algorithm that claims to work at speeds close to RAM limits.
+xxHash is a super fast algorithm that claims to work at speeds close to RAM limits.
 This package provides 32- and 64-bit hash functions.
 As bonus it provides 128-bit hex method.
 
